e6160abdce65fa2c81947c7d081041a1e28d1c36
lingme <kid--l@hotmail.com>
2020-02-13 19:11:11 +0800

    update bird render script

--- Assets/BirdRender.cs
+++ Assets/BirdRender.cs
@@ -6,6 +6,17 @@ public class BirdRender : MonoBehaviour
 {
     private void OnMouseDown()
     {
-        
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
     }
 }
