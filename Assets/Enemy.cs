aed978133211adb8c5e2371158b61674f6b625ff
lingme <kid--l@hotmail.com>
2020-02-14 00:53:06 +0800

    add level script

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _cloudEffect;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         BirdRender birdRender = collision.collider.GetComponent<BirdRender>();
         if (birdRender != null)
         {
+            Instantiate(_cloudEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
             return;
         }
@@ -21,6 +25,7 @@ public class Enemy : MonoBehaviour
 
         if (collision.contacts[0].normal.y < -0.5)
         {
+            Instantiate(_cloudEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
---
+++ Assets/LevelController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var enemy in _enemies)
+        {
+            if(enemy != null)
+            {
+                return;
+            }
+        }
+
+        _nextLevelIndex++;
+        Debug.Log("You Killed all enemies !");
+        SceneManager.LoadScene($"Level{_nextLevelIndex}");
+    }
+}
