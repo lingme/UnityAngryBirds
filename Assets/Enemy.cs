58f6424044cbf751755071e4ff500cb47d46cb05
lingme <kid--l@hotmail.com>
2020-02-14 00:18:17 +0800

    add cloud effects

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,9 +6,22 @@ public class Enemy : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.GetComponent<BirdRender>() != null)
+        BirdRender birdRender = collision.collider.GetComponent<BirdRender>();
+        if (birdRender != null)
         {
+            Destroy(gameObject);
+            return;
+        }
 
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Destroy(gameObject);
         }
     }
 }
